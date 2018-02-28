df82774fcbba91902d1aa5a805da5c4d7aa8bcbc
Almat Juvashev <almatjuvashev@MacBook-Pro-Almat.local>
2018-02-28 15:10:30 +0600

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,44 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, client)=>{
+    if (err) {
+        return console.log("Unable to connect to MongoDB server");
+    }
+    console.log("Connected to MongoDB server");
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos')
+    //                     .find({
+    //                         _id: new ObjectID('5a9538b46664d3ce7ec17be7')
+    //                     })
+    //                     .toArray()
+    //                     .then((docs) => {
+    //                         console.log('Todos');
+    //                         console.log(JSON.stringify(docs, undefined, 2));
+    //                     }, (err) => {
+    //                         console.log("Unable to fetch docs", err);
+    //                     });
+
+    // db.collection('Todos')
+    //                     .find()
+    //                     .count()
+    //                     .then((count) => {
+    //                         console.log(`Todos count: ${count}`);
+    //
+    //                     }, (err) => {
+    //                         console.log("Unable to fetch docs", err);
+    //                     });
+
+    db.collection('Users')
+                        .find({name: "Almat"})
+                        .count()
+                        .then((count) => {
+                            console.log(`Users count: ${count}`);
+
+                        }, (err) => {
+                            console.log("Unable to fetch docs", err);
+                        });
+    //client.close();
+});
